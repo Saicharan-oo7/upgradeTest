cc128ded0005dcada5cde7cb782c271f73dad7eb
Saicharan-oo7 <58461421+Saicharan-oo7@users.noreply.github.com>
2022-03-03 14:29:17 +0530

    Update vigilance.sh

--- vigilance.sh
+++ vigilance.sh
@@ -5,6 +5,6 @@ chmod +x VigilanceMonitorQml
 cd ../
 mkdir softwaretest
 cd softwaretest
-cp -r /home/pi  /home/pi/softwaretest
+cp -r /home/pi/upgradeTest/VigilanceMonitorQml  /home/pi/softwaretest
 echo “Welcome to the Linux family.”
 
