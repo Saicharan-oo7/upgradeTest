fa7b3de4794407788bf311ea2ad043b2e7a9d48f
Saicharan-oo7 <58461421+Saicharan-oo7@users.noreply.github.com>
2022-03-03 14:39:28 +0530

    Update vigilance.sh

--- vigilance.sh
+++ vigilance.sh
@@ -5,7 +5,7 @@ chmod +x VigilanceMonitorQml
 cd ../
 mkdir softwaretest
 cd softwaretest
-cp -r /home/pi/upgradeTest/VigilanceMonitorQml  /home/pi/softwaretest
+yes | cp -rf /home/pi/upgradeTest/VigilanceMonitorQml  /home/pi/Frameless
 echo "software upgrading please wait.."
 echo "rebooting the system"
 sudo reboot
